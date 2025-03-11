4966299d2c55b8d690245a45fdcbb3c5b57726de
Guilherme Lins <guilherme.santos@modalgr.io>
2025-03-11 16:04:46 -0300

    feat: exibir cards buscados na API, criar form-busca service

--- src/app/core/services/promocao.service.ts
+++ src/app/core/services/promocao.service.ts
@@ -1,9 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Promocao } from '../types/type';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PromocaoService {
+  private apiUrl: string = environment.apiUrl;
+
   constructor(private httpCliente: HttpClient) {}
+
+  listar(): Observable<Promocao[]> {
+    return this.httpCliente.get<Promocao[]>(`${this.apiUrl}/promocoes`);
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PromocaoService } from 'src/app/core/services/promocao.service';
+import { Promocao } from 'src/app/core/types/type';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  listaPromocoes!: Promocao[];
 
+  constructor(private promocoesService: PromocaoService) {}
+
+  ngOnInit(): void {
+    this.promocoesService.listar().subscribe({
+      next: (respota) => (this.listaPromocoes = respota),
+    });
+  }
 }
