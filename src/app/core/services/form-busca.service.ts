981fe64fd937632237f46e51ef9e62f3194bffed
Guilherme Lins <guilherme.santos@modalgr.io>
2025-03-11 17:04:03 -0300

    feat: ligar serviço de formulário à opção de passagem

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FormBuscaService {
+  public formBusca: FormGroup;
 
-  constructor() { }
+  constructor() {
+    this.formBusca = new FormGroup({
+      somenteIda: new FormControl(false),
+    });
+  }
 }
