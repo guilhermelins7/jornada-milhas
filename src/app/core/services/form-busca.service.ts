aa3a4207188b20ea0241265953f5e5ce89b80fe5
guilhermelins7 <guilherme7lins@gmail.com>
2025-05-02 18:07:13 -0300

    refactor: adicionar controle do tipo de passagem no form-busca service

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MatChipSelectionChange } from '@angular/material/chips';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from 'src/app/shared/modal/modal.component';
 
 @Injectable({
   providedIn: 'root',
@@ -7,11 +10,12 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class FormBuscaService {
   public formBusca: FormGroup;
 
-  constructor() {
+  constructor(public dialog: MatDialog) {
     this.formBusca = new FormGroup({
       somenteIda: new FormControl(false),
       origem: new FormControl(null),
       destino: new FormControl(null),
+      tipo: new FormControl('Econômica'),
     });
   }
 
@@ -21,5 +25,21 @@ export class FormBuscaService {
       throw new Error(`Controle ${nome} não encontrado no FormGroup`);
     }
     return control as FormControl;
+  }
+
+  alterarTipo(evento: MatChipSelectionChange, tipo: string) {
+    if (evento.selected) {
+      this.formBusca.patchValue({
+        tipo,
+      });
+      console.log('Tipo alterado para:', tipo);
+    }
+  }
+
+  openDialog() {
+    // Abrindo o modal:
+    this.dialog.open(ModalComponent, {
+      width: '50%',
+    });
   }
 }
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,10 +1,11 @@
+import { FormBuscaService } from 'src/app/core/services/form-busca.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
 })
 export class ModalComponent {
-
+  constructor(public formBuscaService: FormBuscaService) {}
 }
