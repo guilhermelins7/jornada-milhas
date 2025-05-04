9f4810bc6ce35c069b5cdd354915bc25d0d28315
guilhermelins7 <guilherme7lins@gmail.com>
2025-05-04 18:48:53 -0300

    refactor: componentizar o seletor de passagens, ateriormente dentro do modal

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -1,3 +1,4 @@
+import { FocusMonitor } from '@angular/cdk/a11y';
 import { Injectable } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatChipSelectionChange } from '@angular/material/chips';
@@ -16,7 +17,36 @@ export class FormBuscaService {
       origem: new FormControl(null),
       destino: new FormControl(null),
       tipo: new FormControl('Econômica'),
+      adultos: new FormControl(1),
+      criancas: new FormControl(0),
+      bebes: new FormControl(0),
     });
+  }
+
+  getDescricaoPassageiros(): string {
+    let descricao = '';
+
+    const adultos = this.formBusca.get('adultos')?.value;
+    const criancas = this.formBusca.get('criancas')?.value;
+    const bebes = this.formBusca.get('bebes')?.value;
+
+    if (adultos && adultos > 0) {
+      descricao += `${adultos} adulto${adultos > 1 ? 's' : ''}`;
+    }
+
+    if (criancas && criancas > 0) {
+      descricao += `${descricao ? ', ' : ''}${criancas} criança${
+        criancas > 1 ? 's' : ''
+      }`;
+    }
+
+    if (bebes && bebes > 0) {
+      descricao += `${descricao ? ', ' : ''}${bebes} bebê${
+        bebes > 1 ? 's' : ''
+      }`;
+    }
+
+    return descricao;
   }
 
   obterControle(nome: string): FormControl {
